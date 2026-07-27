3f9ed2ae14aff51e8eface15ea8f5ebe72f4dffb
Rauan Pinheiro Lima <rauanpinheiro.dip@gmail.com>
2026-07-27 11:26:40 -0300

    linkando páginas do sistema

--- suplementos/urls.py
+++ suplementos/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='dashboard'),
+    path('historico/', views.historico, name='historico'),
+    path('inadimplentes/', views.inadimplentes, name='inadimplentes'),
+    path('orcamento/', views.orcamento, name='orcamento'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('promocoes/', views.promocoes, name='promocoes'),
+    path('validades/', views.validades, name='validades'),
+    path('vendas/', views.vendas, name='vendas'),
 ]
--- suplementos/views.py
+++ suplementos/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'suplementos/pages/index.html')
+    return render(request, 'suplementos/pages/dashboard.html')
+
+def historico(request):
+    return render(request, 'suplementos/pages/historico.html')
+
+def inadimplentes(request):
+    return render(request, 'suplementos/pages/inadimplentes.html')
+
+def orcamento(request):
+    return render(request, 'suplementos/pages/inadimplentes.html')
+
+def produtos(request):
+    return render(request, 'suplementos/pages/produtos.html')
+
+def promocoes(request):
+    return render(request, 'suplementos/pages/promocoes.html')
+
+def validades(request):
+    return render(request, 'suplementos/pages/promocoes.html')
+
+def vendas(request):
+    return render(request, 'suplementos/pages/promocoes.html')
